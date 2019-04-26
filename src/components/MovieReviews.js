115a900e2fec9c5dbbde9a6a940235d9b82c223d
Saint-Jean Germila <germila@noukod.com>
2019-04-26 14:43:30 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+
+class MovieReviews extends Component {
+  // constructor(props) {
+  //   super(props);
+  //
+  //   this state
+  // }
+
+  // render () {
+  //   return (
+  //
+  //   )
+  // }
+}
+
+export default MovieReviews
