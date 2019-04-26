b3db1ce245fc06c4ffb247baf1ddeef2c0b7b4a7
Saint-Jean Germila <germila@noukod.com>
2019-04-26 15:29:22 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,18 +1,24 @@
 // Code MovieReviews Here
-import React, { Component } from 'react';
+import React from 'react'
 
-class MovieReviews extends Component {
-  // constructor(props) {
-  //   super(props);
-  //
-  //   this state
-  // }
+const Review = ({ title, summary }) => (
+  <div className="review">
+    <h3>{ title }</h3>
+    <h4>{ summary }</h4>
+  </div>
+)
 
-  // render () {
-  //   return (
-  //
-  //   )
-  // }
-}
+
+const MovieReviews = ({reviews}) => (
+  <div className="review-list">
+    { reviews.map(review =>
+      <div className="review">
+      <h3>{ review.display_title }</h3>
+      <h4>{ review.summary_short }</h4>
+    </div>) }
+  </div>
+)
+  
+
 
 export default MovieReviews
